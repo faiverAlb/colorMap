7e209d24f12bb0daefd803c54eaad0467a96af9d
faiverAlb <faiver.alb@gmail.com>
2013-07-26 15:37:08 +0400

    Initial commit
    Program on income get path to txt file, on output give html file

---
+++ colorMap.py
@@ -0,0 +1,45 @@
+__author__ = 'Gizetdinov.A'
+
+import itertools
+import sys
+from colour import Color
+from jinja2 import Environment, FileSystemLoader
+
+
+def WriteHtml(colors, outputFile, templateFile):
+    env = Environment(loader=FileSystemLoader(''))
+    template = env.get_template(templateFile)
+    output_from_parsed_template = template.render(colors=colors)
+    with open(outputFile, "w") as fh:
+        fh.write(output_from_parsed_template)
+
+
+def Grouper(n, iterable):
+    args = [iter(iterable)] * n
+    return ([e for e in t if e != None] for t in itertools.zip_longest(*args))
+
+
+def main():
+    if len(sys.argv) != 3:
+        print('usage: ./colorMap.py incomeFile outputHtml')
+        sys.exit(1)
+    incomeFile = sys.argv[1]
+    outputFile = sys.argv[2]
+    templateFile = "mytmplate"
+    groupCount = 30
+    input_file = open(incomeFile)
+    colors = []
+    for line in input_file:
+        words = line.split()
+        for word in words:
+            try:
+                c = Color(word)
+                colors.append(c.get_web())
+            except:
+                pass
+    WriteHtml(list(Grouper(groupCount, colors)), outputFile, templateFile)
+    print('Finished')
+
+
+if __name__ == '__main__':
+    main()
